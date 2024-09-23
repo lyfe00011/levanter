d31cf8e68ef85af4db1ee1827602cbc60e43b9aa
Joshua23-max <joshuashereni17@gmail.com>
2024-09-23 18:28:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || 'koyeb',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
