0e102b5010b19462d6d0363e8c6e3c849f70f4e7
Benjamin62-create <benjaminmirie47@gmail.com>
2024-10-19 05:03:03 +0300

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,38 @@
+const { spawnSync } = require('child_process')
+const { existsSync, writeFileSync } = require('fs')
+
+const SESSION_ID = 'updateThis' // levanter_190f01e329f16c489398dee8cc7b50a88f ' <- this symbol
+
+if (!existsSync('levanter')) {
+  console.log('Cloning the repository...')
+  const cloneResult = spawnSync(
+    'git',
+    ['clone', 'https://github.com/lyfe00011/levanter.git', 'levanter'],
+    {
+      stdio: 'inherit',
+    }
+  )
+
+  if (cloneResult.error) {
+    throw new Error(`Failed to clone the repository: ${cloneResult.error.message}`)
+  }
+
+  const configPath = 'levanter/config.env'
+  try {
+    console.log('Writing to config.env...')
+    writeFileSync(configPath, `VPS=true\nSESSION_ID=${SESSION_ID}`)
+  } catch (err) {
+    throw new Error(`Failed to write to config.env: ${err.message}`)
+  }
+
+  console.log('Installing dependencies...')
+  const installResult = spawnSync('yarn', ['install', '--network-concurrency', '3'], {
+    cwd: 'levanter',
+    stdio: 'inherit',
+  })
+
+  if (installResult.error) {
+    throw new Error(`Failed to install dependencies: ${installResult.error.message}`)
+  }
+}
+spawnSync('node', ['.'], { cwd: 'levanter', stdio: 'inherit' })
