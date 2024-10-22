1c9dda5a2f218c0e0ecd7e4cb462cc61d147b465
lyfe00011 <lyfe00011@gmail.com>
2024-10-22 19:18:53 +0530

    fix

--- test.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require('fs-extra')
-const a = require('./config.js')
-console.log(a)
-let env = { session1: {} }
-for (const b in a) {
-  if (b === 'DATABASE') continue
-  env.session1[b] = a[b]
-}
-writeFileSync('config.json', JSON.stringify(env))
