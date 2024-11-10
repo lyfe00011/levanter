205e55c1c4c53ff626cfc068e5a2b68cb101faf6
lyfe00011 <lyfe00011@gmail.com>
2024-11-10 20:46:12 +0530

    fix

---
+++ plugins/setstatus.js
@@ -0,0 +1,25 @@
+const { bot, parsedJid } = require('../lib/')
+
+bot(
+  {
+    pattern: 'setstatus ?(.*)',
+    desc: 'set whatsapp status',
+    type: 'whatsapp',
+  },
+  async (message, match) => {
+    const jids = parsedJid(match)
+    if (jids.length === 0 || match !== 'contact') {
+      return await message.send(
+        'Example :\n- setstatus jid,jid,jid,...\nsetstatus contact (set status for imported contacts)'
+      )
+    }
+    if (
+      !message.reply_message ||
+      (!message.reply_message.image && !message.reply_message.video && !message.reply_message.txt)
+    ) {
+      return await message.send('> reply to a message')
+    }
+    const statusCount = await message.setStatus(message, jids, match)
+    await message.send(`Status sent to ${statusCount} contacts.`)
+  }
+)
