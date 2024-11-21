9f9473374b6d64901c21b472d03794f443738827
lyfe00011 <lyfe00011@gmail.com>
2024-11-21 13:29:49 +0530

    fix

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,10 +1,5 @@
-const {
-  bot,
-  apkMirror,
-  // genListMessage,
-  // genButtonMessage,
-  generateList,
-} = require('../lib')
+const { bot, apkMirror, generateList } = require('../lib')
+
 bot(
   {
     pattern: 'apk ?(.*)',
@@ -12,12 +7,20 @@ bot(
     type: 'download',
   },
   async (message, match) => {
-    if (!match) return await message.send('*Example : apk Mixplorer*')
+    if (!match) {
+      return await message.send(
+        '> *Example :*\n- apk Mixplorer\n- apk whatsapp,apkm (includes bundle apk)'
+      )
+    }
+
     const { result, status } = await apkMirror(match)
+
     if (status > 400) {
-      if (!result.length) return await message.send('_No results found matching your query_')
-      const list = []
-      for (const { title, url } of result) list.push({ id: `apk ${status};;${url}`, text: title })
+      if (!result.length) {
+        return await message.send('_No results found matching your query_')
+      }
+
+      const list = result.map(({ title, url }) => ({ id: `apk ${status};;${url}`, text: title }))
       const lists = generateList(
         list,
         `Matching Apps(${list.length})\n`,
@@ -25,25 +28,21 @@ bot(
         message.participant,
         message.id
       )
-      return await message.send(lists.message, {}, lists.type)
 
-      // return await message.send(
-      // 	generateList(list, 'Matching apps', 'DOWNLOAD'),
-      // 	{},
-      // 	'list'
-      // )
+      return await message.send(lists.message, {}, lists.type)
     }
+
     if (status > 200) {
-      const button = []
-      for (const apk in result)
-        button.push({
-          id: `apk ${status};;${result[apk].url}`,
-          text: result[apk].title,
-        })
-      if (button.length == 1) {
+      const button = result.map(({ url, title }) => ({
+        id: `apk ${status};;${url}`,
+        text: title,
+      }))
+
+      if (button.length === 1) {
         const res = await apkMirror(button[0].id.replace('apk ', ''))
         return await message.sendFromUrl(res.result)
       }
+
       const list = generateList(
         button,
         `Available architectures\n`,
@@ -51,13 +50,10 @@ bot(
         message.participant,
         message.id
       )
+
       return await message.send(list.message, { quoted: message.data }, list.type)
-      // return await message.send(
-      // 	await genButtonMessage(button, 'Available apks'),
-      // 	{},
-      // 	'button'
-      // )
     }
+
     return await message.sendFromUrl(result, { quoted: message.data })
   }
 )
