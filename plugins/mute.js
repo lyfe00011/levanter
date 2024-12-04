47d7b66348cf59a33b283be1bd841f91d3e094c1
lyfe00011 <lyfe00011@gmail.com>
2024-12-04 19:45:10 +0530

    fix

--- plugins/mute.js
+++ plugins/mute.js
@@ -1,12 +1,4 @@
-const {
-  bot,
-  isAdmin,
-  setMute,
-  addTask,
-  // genButtonMessage,
-  c24to12,
-  getMute,
-} = require('../lib')
+const { bot, isAdmin, setMute, addTask, c24to12, getMute } = require('../lib')
 
 bot(
   {
@@ -49,20 +41,9 @@ bot(
     }
     if (!hour || !min || isNaN(hour) || isNaN(min))
       return await message.send(
-        '*Example : amute 6 0*\namute on | off\namute list\nReply to a text to send Msg on mute'
+        '*Example : amute 6 0*\namute on | off\namute info\nReply to a text to send Msg on mute'
       )
-    // return await message.send(
-    // 	await genButtonMessage(
-    // 		[
-    // 			{ id: 'amute on', text: 'ON' },
-    // 			{ id: 'amute off', text: 'OFF' },
-    // 			{ id: 'amute info', text: 'INFO' },
-    // 		],
-    // 		'*Example : amute 6 0*\namute info\namute on/off\nReply to a text to set Msg'
-    // 	),
-    // 	{},
-    // 	'button'
-    // )
+
     await setMute(message.jid, 'mute', true, message.id, hour, min, msg)
     addTask(message.jid, 'mute', hour, min, msg, message.id)
 
@@ -115,20 +96,9 @@ bot(
     }
     if (!hour || !min || isNaN(hour) || isNaN(min))
       return await message.send(
-        '*Example : aunmute 16 30*\naunmute on | off\naunmute list\nReply to a text to send Msg on unmute'
+        '*Example : aunmute 16 30*\naunmute on | off\naunmute info\nReply to a text to send Msg on unmute'
       )
-    // return await message.send(
-    // 	await genButtonMessage(
-    // 		[
-    // 			{ id: 'aunmute on', text: 'ON' },
-    // 			{ id: 'aunmute off', text: 'OFF' },
-    // 			{ id: 'aunmute info', text: 'INFO' },
-    // 		],
-    // 		'*Example : aunmute 6 0*\naunmute info\naunmute on/off\nReply to a text to set Msg'
-    // 	),
-    // 	{},
-    // 	'button'
-    // )
+
     await setMute(message.jid, 'unmute', true, message.id, hour, min, msg)
     addTask(message.jid, 'unmute', hour, min, msg, message.id)
     return await message.send(
