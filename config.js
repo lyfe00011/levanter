2caae30ee2cb2ad5a48a7457123e565ed98edb24
RanchoMedia <141548801+RanchoMedia@users.noreply.github.com>
2024-12-24 01:06:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
 process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.levanter_2490cdcc1e08f044cda41bc40b8caf9e0d || '').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
