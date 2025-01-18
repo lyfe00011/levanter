d6db23316c1d2c1a1551dda5b89a9f689fe0bd02
BrytBot <brightsite360@gmail.com>
2025-01-18 22:20:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_199cdcf602ab0140c0adb5b5294a0626e4').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
