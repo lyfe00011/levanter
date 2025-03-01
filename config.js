6ed217f0ecff809b5ff194e49c940693cf8a7312
ASHAN68 <ashantharanga68@gmail.com>
2025-03-02 00:59:45 +0530

    Update config.js
    echo "SESSION_ID = Session_Id_you_Got_After_Scan_Dont_Add_This_Line_If_You_Can_Scan_From_Terminal_Itself
    PREFIX = .
    STICKER_PACKNAME = LyFE
    ALWAYS_ONLINE = false
    RMBG_KEY = null
    LANGUAG = en
    WARN_LIMIT = 3
    FORCE_LOGOUT = false
    BRAINSHOP = 159501,6pq8dPiYt7PdqHz3
    MAX_UPLOAD = 200
    REJECT_CALL = false
    SUDO = 989876543210
    TZ = Asia/Kolkata
    VPS = true
    AUTO_STATUS_VIEW = true
    SEND_READ = true
    AJOIN = true
    DISABLE_START_MESSAGE = false
    PERSONAL_MESSAGE = null" > config.env

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || '').trim(levanter_2ce0a8e11728b46f4b363affc905e2cf7),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
