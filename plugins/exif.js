83feec6fb657618e5f41b169fbbc9eb393537936
lyfe00011 <lyfe00011@gmail.com>
2025-03-27 21:18:16 +0530

    fix

--- plugins/exif.js
+++ plugins/exif.js
@@ -7,22 +7,29 @@ bot(
     type: 'sticker',
   },
   async (message) => {
-    if (!message.reply_message || !message.reply_message.sticker) {
-      return await message.send('_Plugin made by manji *USAGE:* Reply to a sticker._')
+    if (!message.reply_message?.sticker) {
+      return await message.send('*Reply to a sticker.*')
     }
 
     const media = await message.reply_message.downloadMediaMessage()
+
+    if (!media) {
+      return await message.send('*Failed to download sticker.*')
+    }
+
     const exifData = await extractExif(media)
 
     if (!exifData) {
-      return await message.send('No Exif metadata found in this sticker.')
+      return await message.send('*No Exif metadata found in this sticker.*')
     }
 
+    const { packId = 'N/A', packname = 'N/A', author = 'N/A', emojis = 'N/A' } = exifData
+
     return await message.send(
-      `*Pack ID:*\n${exifData.packId}\n\n` +
-      `*Pack Name:*\n${exifData.packname}\n\n` +
-      `*Author:*\n${exifData.author}\n\n` +
-      `*Emojis:*\n${exifData.emojis}`,
+      `*Pack ID:* ${packId}\n` +
+        `*Pack Name:* ${packname}\n` +
+        `*Author:* ${author}\n` +
+        `*Emojis:* ${emojis}`,
       { quoted: message.quoted }
     )
   }
