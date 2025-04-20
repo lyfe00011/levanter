9dcaddcb272478c6008e1a49cb19d70003cabede
xSHiNIGAMI1 <105330123+xSHiNIGAMI1@users.noreply.github.com>
2025-04-20 14:10:48 +0000

    Update nr.js
    Would allow you to add commands for a specific demographic like admins only

--- plugins/nr.js
+++ plugins/nr.js
@@ -1,4 +1,4 @@
-const { bot, zushi, yami, ope, jidToNum, lang } = require('../lib/')
+const { getGroupAdmins } = require('../lib/') // adjust as needed
 
 bot(
   {
@@ -7,52 +7,20 @@ bot(
     type: 'logia',
   },
   async (message, match) => {
+    if (message.isGroup) {
+      const admins = await getGroupAdmins(message.jid)
+      if (!admins.includes(message.sender)) {
+        return await message.send("Only *group admins* can use this command.")
+      }
+    }
+
     if (!match) return await message.send(lang.plugins.zushi.usage)
+
     const z = await zushi(match, message.jid, message.id)
     if (!z) return await message.send(lang.plugins.zushi.already_set)
 
     await message.send(
       lang.plugins.zushi.allowed.format(
-        message.isGroup ? message.jid : jidToNum(message.jid),
-        z.map((a) => `- ${a}`).join('\n')
-      ),
-      { contextInfo: { mentionedJid: [message.jid] } }
-    )
-  }
-)
-
-bot(
-  {
-    pattern: 'yami ?(.*)',
-    desc: lang.plugins.yami.desc,
-    type: 'logia',
-  },
-  async (message, match) => {
-    const z = await yami(message.jid, message.id)
-    if (!z || !z.length) return await message.send(lang.plugins.yami.not_set)
-    await message.send(
-      lang.plugins.zushi.allowed.format(
-        message.isGroup ? message.jid : jidToNum(message.jid),
-        z.map((a) => `- ${a}`).join('\n')
-      ),
-      { contextInfo: { mentionedJid: [message.jid] } }
-    )
-  }
-)
-
-bot(
-  {
-    pattern: 'ope ?(.*)',
-    desc: lang.plugins.ope.desc,
-    type: 'logia',
-  },
-  async (message, match) => {
-    if (!match) return await message.send(lang.plugins.ope.usage)
-    const z = await ope(message.jid, match, message.id)
-    if (z === null) return await message.send(lang.plugins.ope.not_found.format(match))
-    if (z === 'all') return await message.send(lang.plugins.ope.all_removed)
-    await message.send(
-      lang.plugins.ope.removed.format(
         message.isGroup ? message.jid : jidToNum(message.jid),
         z.map((a) => `- ${a}`).join('\n')
       ),
