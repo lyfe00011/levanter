8a91c67bc519c6248ce105cb0fab86a2e6217a00
waseemgsaim329 <109344119+waseemgsaim329@users.noreply.github.com>
2025-06-24 03:06:41 +0400

    Create welcome.js

---
+++ plugins/welcome.js
@@ -0,0 +1,17 @@
+{
+  "type": "welcome",
+  "message": "*👋 Welcome to the Group, @user!* \n\n📸 Send your photo below and choose your gender to continue:",
+  "media": "auto", 
+  "buttons": [
+    {
+      "buttonId": "gender_boy",
+      "buttonText": { "displayText": "👦 Boy" },
+      "type": 1
+    },
+    {
+      "buttonId": "gender_girl",
+      "buttonText": { "displayText": "👧 Girl" },
+      "type": 1
+    }
+  ]
+}
