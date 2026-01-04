de50646c8a595be21b2c1dfd874501fcca40569e
lyfe00011 <lyfe00011@gmail.com>
2026-01-04 12:58:48 +0400

    fix

--- plugins/react.js
+++ plugins/react.js
@@ -1,19 +1,39 @@
-const { bot } = require('../lib/')
+const { bot, parseNewsLetterJId } = require('../lib/')
 bot(
-  {
-    pattern: 'react ?(.*)',
-    desc: 'React to msg',
-    type: 'misc',
-  },
-  async (message, match) => {
-    if (!match || !message.reply_message) return await message.send('_Example : react ❣_')
-    return await message.send(
-      {
-        text: match,
-        key: message.reply_message.key,
-      },
-      {},
-      'react'
-    )
-  }
+	{
+		pattern: 'react ?(.*)',
+		fromMe: true,
+		desc: 'React to msg',
+		type: 'misc',
+	},
+	async (message, match) => {
+		if (!match || !message.reply_message)
+			return await message.send('_Example : react ❣_')
+		return await message.send(
+			{
+				text: match,
+				key: message.reply_message.key,
+			},
+			{},
+			'react'
+		)
+	}
+)
+
+bot(
+	{
+		pattern: 'creact ?(.*)',
+		fromMe: true,
+		desc: 'React to channel',
+		type: 'misc',
+	},
+	async (message, match) => {
+		const [url, react] = match.split(',')
+		if (!url || !react) {
+			return await message.send('_Example : creact https://whatsapp.com/channel/xxx/xxx, ❣_')
+		}
+		const { channelId, serverId } = parseNewsLetterJId(url)
+		console.log(channelId, serverId)
+		await message.newsletterReactMessage(channelId, serverId, react)
+	}
 )
