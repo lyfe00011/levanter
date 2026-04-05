4cf6e2b021232787c0a6b779f9a8c3f2fbda748c
lyfe00011 <lyfe00011@gmail.com>
2026-04-05 20:11:08 +0400

    fix

--- plugins/img.js
+++ plugins/img.js
@@ -1,24 +1,32 @@
-const { bot, googleImageSearch } = require('../lib')
+const { bot, pinterestSearch } = require('../lib')
 
 bot(
-  {
-    pattern: 'img ?(.*)',
-    desc: 'Download img from google',
-    type: 'search',
-  },
-  async (message, match) => {
-    if (!match) return await message.send('*Example :  img bot*\n*img 10 bot*')
-    let lim = 3
-    const count = /\d+/.exec(match)
-    if (count) {
-      match = match.replace(count[0], '')
-      lim = count[0]
-    }
-    const result = await googleImageSearch(match)
-    lim = (result.length && (result.length > lim ? lim : result.length)) || result.length
-    await message.send(`_Downloading ${lim} images of ${match.trim()}_`)
-    for (let i = 0; i < lim; i++) {
-      await message.sendFromUrl(result[i], { buffer: false })
-    }
-  }
+	{
+		pattern: 'img ?(.*)',
+		fromMe: true,
+		desc: 'Pinterest image search',
+		type: 'search',
+	},
+	async (message, match) => {
+		if (!match) return await message.send('*Example :  img cats*\n*img 10 cats*')
+		let lim = 3
+		const count = /\d+/.exec(match)
+		if (count) {
+			match = match.replace(count[0], '')
+			lim = parseInt(count[0])
+		}
+
+		const result = await pinterestSearch(match.trim())
+
+		if (!result || !result.length) {
+			return await message.send(`_No Pinterest images found for ${match.trim()}_`)
+		}
+
+		lim = result.length > lim ? lim : result.length
+		await message.send(`_Downloading ${lim} Pinterest images of ${match.trim()}_`)
+
+		for (let i = 0; i < Math.min(lim, result.length); i++) {
+			await message.sendFromUrl(result[i], { buffer: false })
+		}
+	}
 )
